ea1bae1b34b0db8e1740cbcd9ac42320e8294e64
Joshua <147324122+0xh3llO@users.noreply.github.com>
2023-10-10 19:16:51 +0200

    Update avalanchesumhash20.py

--- avalanchesumhash20/avalanchesumhash20.py
+++ avalanchesumhash20/avalanchesumhash20.py
@@ -3,21 +3,21 @@ from math import cos
 import warnings
 
 class AvalancheSumHash20:
-    def __init__(self, input_text, output_length, iterations, modulo_value):
-        self.input_text = input_text
+    def __init__(self, output_length, iterations, modulo_value):
         self.output_length = output_length
         self.iterations = iterations
         self.modulo_value = modulo_value
 
-    def convert_text_to_decimal(self):
-        return sum(ord(char) for char in self.input_text)
+    def convert_text_to_positional_decimal(self, text):
+        decimal_values = [ord(char) * (len(text) - i) ** 2 for i, char in enumerate(text)]
+        return sum(decimal_values)
 
-    def avalanche_sum_hash(self, x):
+    def avalanche_sum_hash(self, x, input_text):
         result = x
 
         for _ in range(self.iterations):
             result = (result ^ x) + (result & x) | (result ^ x)
-            result ^= ord(self.input_text[0])
+            result ^= ord(input_text[0])
 
             for bit in bin(x)[2:]:
                 result = (result << 1) | int(bit)
@@ -36,7 +36,7 @@ class AvalancheSumHash20:
         for digit in str(value)[::-1]:
             shifted_digit = (int(digit) + 7) % 10
             shifted_value += str(shifted_digit)
-        return int(shifted_value) if shifted_value else 0
+        return int(shifted_value)
 
     def concatenate_in_pattern(self, value, num_parts):
         value_str = str(value)
@@ -50,26 +50,30 @@ class AvalancheSumHash20:
             end = (i + 1) * part_size
             parts.append(value_str[start:end])
 
-        concatenated_value = "".join(parts[num_parts//2:] + parts[:num_parts//2])
-        return int(concatenated_value)
+        concatenated_value = "".join(parts[num_parts // 2:] + parts[:num_parts // 2])
+        return int(concatenated_value) % self.modulo_value  # Hier wird die Modulo-Operation hinzugefügt
 
-    def main(self):
-        text_decimal_value = self.convert_text_to_decimal()
+    def encrypt(self, input_text):
+        text_decimal_value = self.convert_text_to_positional_decimal(input_text)
         combined_value = text_decimal_value * self.output_length
-        encrypted_value = self.avalanche_sum_hash(combined_value)
+        encrypted_value = self.avalanche_sum_hash(combined_value, input_text)
         shifted_encrypted_value = self.shift_digits(encrypted_value)
         concatenated_value = self.concatenate_in_pattern(shifted_encrypted_value, 5)
+
+        # Modulo-Operation mit ausreichend großer Zahl
+        concatenated_value %= 10**20
+
         return concatenated_value
 
-# Beispiel für __main__-Bereich
 if __name__ == "__main__":
-    input_text = input("Text: ")
     output_length = 79357612835769157957719395357591595
     iterations = 420
     modulo_value = 93537791153957593571955971579179595
 
-    hash_generator = AvalancheSumHash20(input_text, output_length, iterations, modulo_value)
-    result = hash_generator.main()
+    ash20 = AvalancheSumHash20(output_length, iterations, modulo_value)
+
+    input_text = input("Text: ")
+    encrypted_value = ash20.encrypt(input_text)
 
     print("Eingabe-Text:", input_text)
-    print("Verschlüsselter Wert:", result)
+    print("Verschlüsselter Wert:", encrypted_value)
